e47984fe71aa349fddccb1f527bfbeee0eac9c8a
DumbravaIoanAlexandru <dumbravaalex007@gmail.com>
2023-12-07 18:54:12 +0200

    teme lab 1

---
+++ src/ex4/mainapp.java
@@ -0,0 +1,22 @@
+package ex4;
+
+public class mainapp {
+    public static void main(String[] args) {
+        Random random=new Random();
+        int numar= random.nextInt(20);
+        if(isFibo(numar))
+            System.out.println("Numarul "+numar+" apartine sirului lui Fibonacci");
+        else
+            System.out.println("Numarul "+numar+" nu apartine sirului lui Fibonacci");
+    }
+    public static boolean isFibo(int n){
+        int a=0;
+        int b=1;
+        while(b<n){
+            int urmatorul=a+b;
+            a=b;
+            b=urmatorul;
+        }
+        return b==n;
+    }
+}
